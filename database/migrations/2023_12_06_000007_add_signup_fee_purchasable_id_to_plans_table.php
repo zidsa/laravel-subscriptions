482d83b0783129a39b62f4871bc18d416e24f503
EhabAmawi <ehabamawi98@gmail.com>
2023-12-06 14:49:47 +0300

    add signup_fee_purchasable_id migration and model attribute

---
+++ database/migrations/2023_12_06_000007_add_signup_fee_purchasable_id_to_plans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSignupFeePurchasableIdToPlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table(config('rinvex.subscriptions.tables.app_market_plans'), function (Blueprint $table) {
+            $table->uuid('signup_fee_purchasable_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table(config('rinvex.subscriptions.tables.app_market_plans'), function (Blueprint $table) {
+            $table->dropColumn('signup_fee_purchasable_id');
+        });
+    }
+}
