dcd38d799460125187fe08ccf2e6723c5edb1111
Juan Martinez <pdj1985@gmail.com>
2023-12-14 09:18:07 +0100

    added discount and type to rules for saving

--- src/Models/AppMarketPlanOffers.php
+++ src/Models/AppMarketPlanOffers.php
@@ -91,6 +91,8 @@ class AppMarketPlanOffers extends Model
             'price' => 'required|numeric',
             'offer_period' => 'sometimes|integer|max:100000',
             'offer_interval' => 'sometimes|in:hour,day,week,month',
+            'discount' => 'nullable|integer',
+            'type' => 'nullable|string',
         ]);
     }
 
